df13c01e3c078922f812668fedc1ae5068b71acd
Deep Gandhi <deepgandhi151@gmail.com>
2022-03-24 22:49:31 +0530

    Time: 96 ms (70.24%), Space: 41.9 MB (91.53%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.cpp
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -5,22 +5,19 @@ public:
         int boat =0;
         int n=people.size();
         int end = n-1;
-        for(int i=0;i<n;i++){
-            // cout<<"i: "<<i<<endl<<"end: "<<end<<endl<<"people i: "<<people[i]<<endl<<"people end: "<<people[end]<<endl;
-            // cout<<(people[i]!=0&&people[i]+people[end]<=limit)<<endl;
-            if(people[i]!=0&&people[i]+people[end]<=limit){
-                people[end]=0;
-                people[i]=0;
-                boat++;
+        int i = 0;
+        // for(int i=0;i<n;i++){
+        while(i<=end){
+            if(people[i]+people[end]<=limit){
+
                 end--;
             }
-            else if(people[i]!=0){
-                 people[i]=0;
                 boat++;
-            }
-            
-            
+                i++;
         }
+            
+            
+        
         return boat;
     }
 };
