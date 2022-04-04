db0611f898cc6d562d85c0a0e7e75ec3c87b9d50
Deep Gandhi <deepgandhi151@gmail.com>
2022-04-04 10:06:12 +0530

    Time: 887 ms (50.07%), Space: 180.1 MB (89.18%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -1,52 +1,37 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) {
-        int first=101,second=101;
-        int fC=1; int count = 0; int bC=1;
-        ListNode* node = head;
-        while(node!=NULL){
+        ListNode* firstnode = head;
+        ListNode* secondnode = head;
+        ListNode* current = head;
+        int count = 0;
+        
+        if(head == nullptr || head->next == nullptr) return head;
+        
+        while(current){
+            current = current->next;
             count++;
-            node=node->next;
         }
-        if(count == 1) return head;
-        node = head;
-        while(node!=NULL){
-            if(fC == k || fC == count+1-k){
-                if(first ==101){
-                    first = node->val;
-                }else{
-                    second = node -> val;
-                    node->val = first;
-                }
-            } 
-            node=node->next;
-            fC++;
-        }
-        node = head;
-        while(node!=NULL){
-            int x =min(k,count+1-k);
-            if(bC==x ){
-                if(second !=101){
-                    node->val=second;
-                }else{
-                    node->val=first;
-                }
-            }
-            bC++;
-            node=node->next;
+        int num;
+        num = count-k+1;
+        
+        while(k>1){
+            firstnode = firstnode->next;
+            k--;
         }
         
-        cout<<count;
+        while(num>1){
+            secondnode = secondnode->next;
+            num--;
+        }
+        
+        int temp ;
+        temp = firstnode->val;
+        firstnode->val = secondnode->val;
+        secondnode->val = temp;
+        
         return head;
+        
+        
     }
 };
