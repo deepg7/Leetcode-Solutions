b020487142ab45e0f2752061eefdddc34a0d7d79
Deep Gandhi <deepgandhi151@gmail.com>
2022-04-16 11:10:30 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (55.49%) - LeetHub

---
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    int sum = 0;
+    TreeNode* bstToGst(TreeNode* root) {
+        if(root!=NULL)
+        {
+            bstToGst(root->right);
+            sum+=root->val;
+            root->val = sum;
+            bstToGst(root->left);
+        }
+        return root;
+        
+    }
+};
+// class Solution {
+// public:
+    
+//     vector<int> nums;
+//     void printInorder(TreeNode* node){
+//         if (node == NULL)
+//             return;
+
+//         printInorder(node->left);
+
+//         cout << node->val << " ";
+//         nums.push_back(node->val);
+
+//         printInorder(node->right);
+//     }
+    
+//     void addInorder(TreeNode* node){
+//         if (node == NULL)
+//             return;
+
+//         addInorder(node->left);
+//         int sum=0;
+//         int val = node->val;
+//         cout << node->val << " ";
+//         for(auto it:nums){
+//             if(it>val)sum+=it;
+//         }
+//         cout<<val+sum;
+//         node->val = val + sum;
+
+//         addInorder(node->right);
+//     }
+//     TreeNode* convertBST(TreeNode* root) {
+//         TreeNode* node = root;
+//         printInorder(node);
+//         std::reverse(nums.begin(),nums.end());
+//         addInorder(node);
+//         return root;
+//     }
+// };
