6d2bf5c43a36d40d06abb93dc79221ca8a7ae82e
Deep Gandhi <deepgandhi151@gmail.com>
2022-06-16 17:47:28 +0530

    Time: 5 ms (37.38%), Space: 9 MB (89.57%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+//         sort(nums1.begin(),nums1.end());
+//         sort(nums2.begin(),nums2.end());
+        
         int i = 0;
-        int j = 0;
-        for(int i =0;i<n;i++){
-            nums1[i+m]=nums2[i];
+        if(n==0) return;
+        while(i<m){
+            if(nums1[i]>nums2[0]) {
+                swap(nums1[i],nums2[0]);
+                sort(nums2.begin(),nums2.end());
+            }
+            i++;
         }
-        sort(nums1.begin(),nums1.end());
+        for(int j = 0;j<n;j++){
+            nums1[m+j]=nums2[j];
+        }
         
     }
 };
