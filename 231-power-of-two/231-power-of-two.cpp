68f59d13023fc9da78db8f01eda2399dbdb91f10
Deep Gandhi <deepgandhi151@gmail.com>
2022-06-24 15:55:23 +0530

    Time: 3 ms (57.65%), Space: 6 MB (24.95%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
+
     bool isPowerOfTwo(int n) {
-        if(n==0)return false;
-       return dp(n);
+        if(n<=0) return false;
+        return ((n&(n-1))==0);
     }
+//     bool isPowerOfTwo(int n) {
+//         if(n==0)return false;
+//        return dp(n);
+//     }
     
-    bool dp(int n){
-        if(n==1)return true;
-        else if (n%2!=0) return false;
-        else return dp(n/2);
-    }
+//     bool dp(int n){
+//         if(n==1)return true;
+//         else if (n%2!=0) return false;
+//         else return dp(n/2);
+//     }
 };
