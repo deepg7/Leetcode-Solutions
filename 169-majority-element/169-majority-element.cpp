a9eb2c6e5ac4347b914017d276bb41e03ccb38e1
Deep Gandhi <deepgandhi151@gmail.com>
2022-06-28 16:56:03 +0530

    Time: 32 ms (39.70%), Space: 19.7 MB (13.20%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -2,10 +2,21 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         map<int,int> check;
-        for(auto it: nums) check[it]++;
-        for(auto it: check){
-            if(it.second > nums.size()/2) return it.first;
-        }
+        for(auto it: nums){
+            check[it]++;
+            if(check[it]>nums.size()/2)return it;
+        } 
+        // for(auto it: check){
+        //     if(it.second > nums.size()/2) return it.first;
+        // }
         return 0;
+        
+        // O(n) time and space (brute)
+        // map<int,int> check;
+        // for(auto it: nums) check[it]++;
+        // for(auto it: check){
+        //     if(it.second > nums.size()/2) return it.first;
+        // }
+        // return 0;
     }
 };
