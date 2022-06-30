536f0e484fb4c31a269a32eec43455083ff5b273
Deep Gandhi <deepgandhi151@gmail.com>
2022-06-30 11:31:27 +0530

    Time: 39 ms (54.07%), Space: 22.4 MB (8.92%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -2,20 +2,27 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
 
-        int ans = 0;
-        for (auto it: nums) ans ^=it;
-        return ans;
-//         int start = 0;
-//         int end = nums.size()-1; 
-//         int mid = start + (start - end)/2;
-//         while(start<=end){
-//             if(mid!=0 && mid!=nums.size()-1) {
-//                 if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]) return nums[mid];
-                
-//             }else if(mid!=0 || mid!=nums.size()-1){
-//                 if(nums[mid]!=nums[mid-1]) return nums[mid];
-//             }else 
-//         }
-//         return 0;
+        //XOR APPROACH SPACE O(1) TIME O(N)
+        // int ans = 0;
+        // for (auto it: nums) ans ^=it;
+        // return ans;
+        
+        int low = 0;
+        int high = nums.size()-1;
+        if(high == 0) return nums[0];
+        int mid;
+        while(low<=high){
+            mid = low + (high-low)/2;
+            
+            if(mid%2==0){
+                if(nums[mid]!=nums[mid+1]){
+                    high = mid-1;
+                }else low = mid+1;
+            }else {
+                if(nums[mid]==nums[mid+1]) high = mid -1;
+                else low = mid+1;
+            }
+        }
+        return nums[low];
     }
 };
