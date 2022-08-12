dd9623785f8910072f3c7223fdd79e7c63688426
Deep Gandhi <deepgandhi151@gmail.com>
2022-08-12 22:36:53 +0530

    Time: 535 ms (50.71%), Space: 101.6 MB (65.19%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -1,27 +1,43 @@
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
-        int curr_sum=0, res=0;
+//         int curr_sum=0, res=0;
 		
-		//set to store the elements
-        unordered_set<int> st;
+// 		//set to store the elements
+//         unordered_set<int> st;
         
-        int i=0,j=0;
-        while(j<nums.size()) {
-            while(st.count(nums[j])>0) {
-				//Removing the ith element untill we reach the repeating element
-                st.erase(nums[i]);
-                curr_sum-=nums[i];
-                i++;
-            }
-			//Add the current element to set and curr_sum value
-            curr_sum+=nums[j];
-            st.insert(nums[j++]);
+//         int i=0,j=0;
+//         while(j<nums.size()) {
+//             while(st.count(nums[j])>0) {
+// 				//Removing the ith element untill we reach the repeating element
+//                 st.erase(nums[i]);
+//                 curr_sum-=nums[i];
+//                 i++;
+//             }
+// 			//Add the current element to set and curr_sum value
+//             curr_sum+=nums[j];
+//             st.insert(nums[j++]);
 			
-			//res variable to keep track of largest curr_sum encountered till now...
-            res = max(res, curr_sum);
-        }
+// 			//res variable to keep track of largest curr_sum encountered till now...
+//             res = max(res, curr_sum);
+//         }
         
-        return res;
+//         return res;
+        map<int,int> check;
+        int i =0;
+        int j = 0;
+        int ans = 0;
+        int curr = 0;
+        int N = nums.size();
+        for(;j<N;++j){
+            check[nums[j]]++;
+            curr+=nums[j];
+            while(check[nums[j]]>1){
+                check[nums[i]]--;
+                curr-=nums[i++];
+            }
+            ans = max(curr,ans);
+        }
+        return ans;
     }
 };
