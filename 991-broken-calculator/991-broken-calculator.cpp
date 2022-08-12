65e9fa52b0f4a6dd49d171be47f8a2ab77d0a4c2
Deep Gandhi <deepgandhi151@gmail.com>
2022-08-12 22:56:29 +0530

    Time: 4 ms (27.11%), Space: 5.9 MB (26.48%) - LeetHub

--- 991-broken-calculator/991-broken-calculator.cpp
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
-    int brokenCalc(int X, int Y) {
+    int brokenCalc(int startValue, int target) {
         int ans = 0;
-    while (X < Y) {
-        ans++;
-        if (Y % 2) Y++;
-        else Y /= 2;
-    }
-    return X - Y + ans;
+        while (target > startValue) {
+            ans++;
+            if (target % 2 == 1)
+                target++;
+            else
+                target /= 2;
+        }
+
+        return ans + startValue - target;
     }
 };
